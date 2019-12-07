c15f28b6bc4fd9ba1ebc7c5aba2873d68c18585e
wleja <wojjttek@gmail.com>
2019-12-07 22:32:42 +0100

    Dodaj @NoArgsConstructor

--- src/main/java/pl/sztukakodu/works/tasks/boundary/CreateTaskRequest.java
+++ src/main/java/pl/sztukakodu/works/tasks/boundary/CreateTaskRequest.java
@@ -2,9 +2,11 @@ package pl.sztukakodu.works.tasks.boundary;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class CreateTaskRequest {
     private String title;
     private String author;
